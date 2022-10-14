586dd7ff86aa1ffb7d0e538df90a312a89b3b8f3
vlasvlasvlas <vlasvlasvlas@gmail.com>
2022-10-14 08:57:49 -0300

    black fixed

--- profiling/profiling.py
+++ profiling/profiling.py
@@ -16,13 +16,14 @@ Returns:
 """
 
 # data folder
-datafolder = 'data-in/'
+datafolder = "data-in/"
 
 # html pandas profiling output folder
-dataprofiles = 'data-profiles/'
+dataprofiles = "data-profiles/"
 
 # file extension to filter in data folder (could be a list also ['ext1','ext2'] )
-extensions = (".csv",".xlsx")
+extensions = (".csv", ".xlsx")
+
 
 def find_csv_filenames(path_to_dir, suffix=extensions):
     """_summary_
@@ -35,22 +36,23 @@ def find_csv_filenames(path_to_dir, suffix=extensions):
         _type_: _description_
     """
     filenames = listdir(path_to_dir)
-    
-    return [ filename for filename in filenames if filename.endswith( suffix ) ]
+
+    return [filename for filename in filenames if filename.endswith(suffix)]
+
 
 datafiles = find_csv_filenames(datafolder)
 
 for datafile in datafiles:
-    
-    dataname = datafile.split('.')[0]
-    dataext = datafile.split('.')[-1]
-    datafile = datafolder+datafile
-    
-    if dataext.lower() == 'xlsx':
-        df = pd.read_excel(datafile,index_col=False, engine='openpyxl') # XLSX
-    if dataext.lower() == 'csv':
-        df = pd.read_csv(datafile,low_memory=False,index_col=False) # CSV
+
+    dataname = datafile.split(".")[0]
+    dataext = datafile.split(".")[-1]
+    datafile = datafolder + datafile
+
+    if dataext.lower() == "xlsx":
+        df = pd.read_excel(datafile, index_col=False, engine="openpyxl")  # XLSX
+    if dataext.lower() == "csv":
+        df = pd.read_csv(datafile, low_memory=False, index_col=False)  # CSV
     print(df)
 
     profile = ProfileReport(df, title=f"{dataname} Profiling Report", minimal=True)
-    profile.to_file(f'{dataprofiles+dataname}.html')
+    profile.to_file(f"{dataprofiles+dataname}.html")
